dfe838dd7cdfe72922db262857103331d15eb265
hairui1986sd <hairui1986sd@163.com>
2023-04-16 22:47:20 +0800

    删除无用import

--- src/main/java/com/jerico/springboot/mybatis/util/ResponseAdvice.java
+++ src/main/java/com/jerico/springboot/mybatis/util/ResponseAdvice.java
@@ -10,20 +10,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 /**
- * 统一处理响应体，用ResponseUtil.success静态方法包装，
- * 在API接口使用时就可以直接返回原始类型.
+ * 统一处理响应体，用ResponseUtil.success静态方法包装， 在API接口使用时就可以直接返回原始类型.
  */
 //@RestControllerAdvice
 public class ResponseAdvice implements ResponseBodyAdvice<Object> {
 
     /**
      * 是否支持advice功能
-     * @param returnType
-     * @param converterType
+     *
+     * @param returnType 返回类型
+     * @param converterType 转换类型
      * @return true 支持，false 不支持
      */
     @Override
@@ -35,10 +34,12 @@ public class ResponseAdvice implements ResponseBodyAdvice<Object> {
      * 对返回的数据进行处理
      */
     @Override
-    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+        Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request,
+        ServerHttpResponse response) {
         // 正常响应则返回ResponseUtil中方法包装响应类；异常响应会走到全局异常处理(当然在未写全局异常捕获与处理时，异常也会走到这里)。
         // 如果响应体是ResponseDTO包装后的，直接返回。不要重复封装
-            if (body instanceof ResponseDTO) {
+        if (body instanceof ResponseDTO) {
             return body;
         }
 
